d27d804d3fb5639daf6ba424bddb8b8074d3c5c1
aditya <adityakadali00@gmail.com>
2022-10-02 15:31:07 +0530

    project structured

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primay: "#0B0D17",
+        secondary: "#D0D6F9",
+      },
+    },
   },
   plugins: [],
 };
